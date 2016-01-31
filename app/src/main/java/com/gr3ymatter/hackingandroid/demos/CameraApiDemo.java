25ca441bdeaf42b5049e2774c5e0c36b557a25ed
Gr3yMatter <sal.afzal@gmail.com>
2016-01-31 13:20:01 -0800

    Added Concept Class to Hold Concept Information Implemented OnItemClickListener of GridView to Launch Activity using the string name stored in the Concepts File.

---
+++ app/src/main/java/com/gr3ymatter/hackingandroid/demos/CameraApiDemo.java
@@ -0,0 +1,19 @@
+package com.gr3ymatter.hackingandroid.demos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.gr3ymatter.hackingandroid.R;
+
+/**
+ * Created by Afzal on 1/31/16.
+ */
+public class CameraApiDemo extends Activity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_notes);
+    }
+}
